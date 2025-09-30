d2e81fcabe8e0804faee27d8ca575753ec5c98da
parusan-partheepan-ics4u1-01-2025 <parusan.partheepan@student.tdsb.on.ca>
2025-09-30 18:21:26 +0000

    make changes done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,37 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-	}
+    }
 
+
+
+//Question 1
+public static int goodString(String s1) {
+    int len = s1.length();
+    int count = Integer.MAX_VALUE; 
+
+    char firstLetter = s1.charAt(0);
+    char lastLetter = s1.charAt(len - 1);
+
+    if (len >= 2 && firstLetter == lastLetter) {
+        return 0;
+    }
+
+    for (int i = 0; i < len; i++) {
+        for (int back = len - 1; back > i; back--) {
+            if (s1.charAt(i) == s1.charAt(back)) {
+                int operations = i + (len - 1 - back);
+                if (back - i + 1 >= 2 && operations < count) {
+                    count = operations;
+                }
+            }
+        }
+    }
+
+    if (count == Integer.MAX_VALUE) {
+        return -1;
+    }
+    return count;
+}
 }
